84463fac6d20677ec2cda9058de4179609f62259
Mudit Garg <muditgarg48@gmail.com>
2024-01-27 02:05:45 +0530

    Changing navigation nature

--- lib/tools/navigation.dart
+++ lib/tools/navigation.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void goTo(Widget nextPage, BuildContext context) {
-  Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>nextPage));
+  Navigator.of(context).push(MaterialPageRoute(builder: (context)=>nextPage));
 }
 
 void goBack(BuildContext context) {
