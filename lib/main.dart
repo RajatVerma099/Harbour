d2071b625927b57d48357c94159fa992a44b308c
Mudit Garg <muditgarg48@gmail.com>
2024-01-27 01:33:13 +0530

    Making small adjustment to the folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
-import 'firebase_options.dart';
+import 'firebase/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:harbour/show_data.dart';
 
