4abbc2c5e62d1bff5b5ac1e7c47facb3fbd3e2bd
RAJAT_VERMA <rv5393982@gmail.com>
2024-01-28 15:48:03 +0530

    previous + frosted glass header

--- lib/pages/resume.dart
+++ lib/pages/resume.dart
@@ -7,7 +7,14 @@ void main() {
   ));
 }
 
-class Resume extends StatelessWidget {
+class Resume extends StatefulWidget {
+  const Resume({Key? key}) : super(key: key);
+
+  @override
+  _ResumeState createState() => _ResumeState();
+}
+
+class _ResumeState extends State<Resume> {
   final List<String> imageUrls = [
     'https://drive.google.com/uc?export=view&id=1sTfin3hQVKT2WKFpSHaCznt-W-f8XGnA',
     'https://drive.google.com/uc?export=view&id=1sU1sa4NThCu4Z6FiGKb5K7p09TYS12TW',
@@ -21,7 +28,14 @@ class Resume extends StatelessWidget {
     'https://drive.google.com/uc?export=view&id=1slROfIV1XJU5iRrSumYVao2iwiS9c2pn',
   ];
 
-  Resume({super.key});
+  late CarouselController _controller;
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CarouselController();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,33 +43,97 @@ class Resume extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Card Viewer'),
       ),
-      body: Center(
-        child: CarouselSlider.builder(
-          itemCount: imageUrls.length,
-          options: CarouselOptions(
-            height: 600.0,
-            enableInfiniteScroll: false,
-            viewportFraction: 0.8,
-            enlargeCenterPage: true,
-          ),
-          itemBuilder: (context, index, realIndex) {
-            return Container(
-              width: double.infinity,
-              child: Card(
-                elevation: 16.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16.0),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          CarouselSlider.builder(
+            carouselController: _controller,
+            itemCount: imageUrls.length,
+            options: CarouselOptions(
+              height: 600.0,
+              enableInfiniteScroll: false,
+              viewportFraction: 0.8,
+              enlargeCenterPage: true,
+              pageSnapping: false,
+              scrollPhysics: BouncingScrollPhysics(),
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+            ),
+            itemBuilder: (context, index, realIndex) {
+              return Container(
+                width: double.infinity,
+                child: Card(
+                  elevation: 16.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.zero,
+                    child: Image.network(
+                      imageUrls[index],
+                      fit: BoxFit.contain,
+                    ),
+                  ),
                 ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(16.0),
-                  child: Image.network(
-                    imageUrls[index],
-                    fit: BoxFit.cover,
+              );
+            },
+          ),
+          Positioned(
+            top: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child: FrostedGlassHeader(),
+          ),
+          Positioned(
+            bottom: 16.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                imageUrls.length,
+                    (index) => Container(
+                  width: 8.0,
+                  height: 8.0,
+                  margin: EdgeInsets.symmetric(horizontal: 4.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white.withOpacity(index == _currentIndex ? 1.0 : 0.4),
                   ),
                 ),
               ),
-            );
-          },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FrostedGlassHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100.0,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Colors.white.withOpacity(0.2), Colors.white.withOpacity(0.5)],
+        ),
+      ),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            'Tap to Download',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
         ),
       ),
     );
